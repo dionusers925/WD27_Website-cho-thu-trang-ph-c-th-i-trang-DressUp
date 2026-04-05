cf27d283de859efc48fcb75d088fa20c9f85b991
dionusers925 <phamhonggiang11122004@gmail.com>
2026-04-05 08:21:04 +0700

    update code

--- Client/src/layouts/ProtectedRoute.tsx
+++ Client/src/layouts/ProtectedRoute.tsx
@@ -1,15 +1,21 @@
-
 import { Outlet, Navigate } from "react-router-dom";
 
 const ProtectedRoute = () => {
   const token = localStorage.getItem("token");
+  const user = JSON.parse(localStorage.getItem("user") || "{}");
 
+  // ❌ chưa đăng nhập
   if (!token) {
     return <Navigate to="/auth/login" replace />;
   }
 
+  // ❌ không phải admin
+  if (user.role !== "admin") {
+    return <Navigate to="/" replace />;
+  }
+
+  // ✅ là admin
   return <Outlet />;
 };
 
 export default ProtectedRoute;
-
